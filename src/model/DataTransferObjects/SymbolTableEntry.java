99e2bcd8dcf5edd1f9e5d6240f94f716bbc6f8ce
914_Ionel_Lavinia <67369509+lavinia06@users.noreply.github.com>
2024-03-05 11:41:18 +0200

    Add files via upload

---
+++ src/model/DataTransferObjects/SymbolTableEntry.java
@@ -0,0 +1,42 @@
+package model.DataTransferObjects;
+
+import com.sun.jdi.Value;
+import javafx.beans.property.SimpleStringProperty;
+import model.value.ValueInterface;
+
+public class SymbolTableEntry
+{
+    private SimpleStringProperty variableName;
+    private SimpleStringProperty value;
+
+
+    public SymbolTableEntry(String variableName, ValueInterface value){
+        this.variableName = new SimpleStringProperty(variableName);
+        this.value = new SimpleStringProperty(value.toString());
+    }
+
+    public SimpleStringProperty variableNameProperty(){
+        return this.variableName;
+    }
+
+    public SimpleStringProperty valueProperty(){
+        return this.value;
+    }
+
+    public String getVariableName(){
+        return this.variableName.get();
+    }
+
+    public String getValue(){
+        return this.value.get();
+    }
+
+    public void setVariableName(String newVariableName){
+        this.variableName.set(newVariableName);
+    }
+
+    public void setValue(String newValue){
+        this.value.set(newValue);
+    }
+
+}
