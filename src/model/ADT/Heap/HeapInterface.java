2cc07852f7fecb4c0fc8ee03b556efe145d66379
914_Ionel_Lavinia <67369509+lavinia06@users.noreply.github.com>
2024-03-05 11:52:49 +0200

    Add files via upload

---
+++ src/model/ADT/Heap/HeapInterface.java
@@ -0,0 +1,25 @@
+package model.ADT.Heap;
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface HeapInterface <TKey, TValue>
+{
+
+    public Collection<TValue> values();
+    public void setContent(Map<TKey, TValue> newHeap);
+    public void setFirstAvailablePosition();
+    public int getFirstAvailablePosition();
+    public int size();
+    public boolean containsKey(TKey key);
+    public boolean containsValue(TValue value);
+    public boolean isEmpty();
+    public void update(TKey key, TValue newValue);
+    public void insert(TKey key, TValue newValue);
+    public void clear();
+    public TValue getValue(TKey key);
+    public TValue remove(TKey key);
+    public Collection<TValue> getAllValues();
+    public Collection<TKey> getAllKeys();
+    public Map<TKey, TValue> getContent();
+}
